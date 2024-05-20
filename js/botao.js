6b4debaf709f8e0609a87189fd124d5594c0266b
Arthur Tavares Carvalho <t.arthur@zap2.local>
2024-05-20 20:33:48 -0300

    Commit message

--- js/botao.js
+++ js/botao.js
@@ -1,22 +1,28 @@
+// Função para abrir uma aba específica e marcar como ativa
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
-
+    
+    // Esconde todos os conteúdos das abas
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-
+ 
+    // Remove a classe 'active-tab' de todos os links de aba
     tablinks = document.getElementsByClassName("tab");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active-tab", "");
     }
-
+    
+    // Mostra o conteúdo da aba clicada e marca o link como ativo
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active-tab";
 }
 
+// Simula o clique no primeiro link de aba ao carregar a página
 document.getElementsByClassName("tab")[0].click();
 
+// Adiciona um listener para o botão de enviar no formulário
 document.getElementById('enviar').addEventListener('click', function() {
     // Captura dos dados do formulário
     var nome = document.getElementById('nome').value;
